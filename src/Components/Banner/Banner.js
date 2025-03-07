f88a150773c59d17cd1d2b8c4d3c5d0ebd2cb90b
ROVIN K SUDHAKARAN <rovinksudhakaran@gmail.com>
2025-03-07 15:45:21 +0530

    Fetch and display banner contents using Axios and API

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY, imageUrl} from '../../Constants/Constants'
+import axios from '../../Axios'
 import './Banner.css'
 
 function Banner() {
+  console.log("api:",API_KEY)
+    const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) =>  {
+          setMovie(response.data.results[1])
+       })
+  }, [])
   return (
-    <div className='banner'>
+    <div 
+    style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+    className='banner'>
       <div className='content'>
-        <h1 className='title'>Movie Name</h1>
+        <h1 className='title'> {movie ? (movie.name || movie.title) : ""} </h1>
         <div className='banner_buttons'>
             <button className='button'>Play</button>
             <button className='button'>My list</button>
         </div>
-        <h1 className='discription'>Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit ipsum felis commodo lectus nisl aliquam fusce.</h1>
+        <h1 className='discription'>{movie ? movie.overview : ""} </h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
